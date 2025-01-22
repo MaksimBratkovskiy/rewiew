52ce6b138ed5e254e43153b24222096ca261e83d
ALT1\m.bratkovskii <m.bratkovskii@a1systems.com>
2025-01-22 20:32:43 +0300

    add features

--- study_for_tbank/src/main/java/org/example/collectionFrame/Person.java
+++ study_for_tbank/src/main/java/org/example/collectionFrame/Person.java
@@ -1,4 +1,55 @@
 package org.example.collectionFrame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
+
+    public enum Sex {
+        MALE, FEMALE;
+    }
+
+    String name;
+    int age;
+    Sex gender;
+    String emailAddress;
+
+    public int getAge(){
+        return age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String emailAddress(){
+        return emailAddress;
+    }
+
+    public Sex getGender(){
+        return gender;
+    }
+
+    public static void main(String[] args) {
+        List<Person> roster = new ArrayList<>();
+
+        for (Person p : roster) {
+            System.out.println(p.getName());
+        }
+
+        roster
+                .stream()
+                .filter(e -> e.getGender() == Sex.FEMALE)
+                .forEach(e -> System.out.println(e.getName()));
+
+        double doubleRoster = roster
+                .stream()
+                .filter(e -> e.getGender() == Sex.MALE)
+                .mapToInt(Person::getAge)
+                .average()
+                .getAsDouble();
+
+
+    }
+
 }
