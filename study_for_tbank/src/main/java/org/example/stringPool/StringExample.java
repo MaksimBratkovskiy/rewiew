fd59adb1ac3226c17b8d0ed1b7cbff7b9dab6bd8
ALT1\m.bratkovskii <m.bratkovskii@a1systems.com>
2025-01-28 18:12:19 +0300

    change

---
+++ study_for_tbank/src/main/java/org/example/stringPool/StringExample.java
@@ -0,0 +1,11 @@
+package org.example.stringPool;
+
+public class StringExample {
+    final static String once = new String("once");
+    final static String two = new String("once");
+
+    public static void main(String[] args){
+        System.out.println();
+    }
+
+}
